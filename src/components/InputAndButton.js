a18d1dba3351cc4d3d8d7e7169af1e44d019aa62
zhoutk <zhoutk@189.cn>
2017-04-27 17:32:15 +0800

    示例三重构，输入组件去掉state，由refs完成键盘与鼠标的操作统一。

--- src/components/InputAndButton.js
+++ src/components/InputAndButton.js
@@ -8,23 +8,16 @@ import {TodoButton, TodoInput} from '../styled/Todo';
 class InputAndButton extends Component {
     constructor(props)	{
         super(props);
-        this.state={
-            inputValue:''
-        };
     }
     focus = () => {
         this.el.focus();
     };
-    onChange = e => {
-        this.setState({
-            inputValue:e.target.value
-        })
-    };
     addItem = e => {
         if(e.which === 13 || e.type === 'click'){
-            if(this.state.inputValue.trim().length > 0) {
-                this.props.onSave(this.state.inputValue.trim());
+            if(e.target.value.trim().length > 0 || this.el.value.trim().length > 0) {
+                this.props.onSave(e.target.value.trim() || this.el.value.trim());
                 e.target.value = '';
+                this.el.value = '';
                 this.setState({
                     inputValue:''
                 })
@@ -36,10 +29,8 @@ class InputAndButton extends Component {
         return (
             <div>
                 <TodoInput innerRef={el=> { this.el = el; }}
-                           onChange={this.onChange}
                            onKeyUp={this.addItem}
-                           placeholder="enter task"
-                           value={this.state.inputValue}>
+                           placeholder="enter task" >
                 </TodoInput>
                 <TodoButton onClick={this.addItem} type="submit">add</TodoButton>
             </div>
