50eee61d071261144dc046eb8a063d7d3c484e99
zhoutk <zhoutk@189.cn>
2017-04-27 15:20:52 +0800

    LiList -> 函数式组件

--- src/components/LiList.js
+++ src/components/LiList.js
@@ -6,16 +6,14 @@ import React, { Component } from 'react';
 import {TodoUl} from '../styled/Todo';
 import Li from './Li';
 
-class LiList extends Component {
-    render() {
-        return (
-            <TodoUl>
-                {this.props.items.map((item)=>{
-                    return <Li {...item}></Li>;
-                })}
-            </TodoUl>
-        );
-    }
-}
+const LiList = (props)=>{
+    return (
+        <TodoUl>
+            {props.items.map((item)=>{
+                return <Li {...item}></Li>;
+            })}
+        </TodoUl>
+    );
+};
 
 export default LiList;
