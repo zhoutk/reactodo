71cdcde1fe49f1b013c62a9ab1676645035330ad
zhoutk <zhoutk@189.cn>
2017-05-02 17:50:31 +0800

    redux component form is ok.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,26 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import InputAndButton from '../components/InputAndButton';
 import LiList from '../components/LiList';
 import * as TodoActions from '../actions';
 
-const App = ({todos, actions}) => (
-    <div>
-        <InputAndButton onSave={actions.addTodo}/>
-        <LiList items={todos}/>
-    </div>
-);
-
-App.propTypes = {
-    todos: PropTypes.array.isRequired,
-    actions: PropTypes.object.isRequired
+class App extends Component {
+    static propTypes = {
+        todos: PropTypes.array.isRequired,
+        actions: PropTypes.object.isRequired
+    };
+    componentDidMount (){
+        this.InputComponent.focus();
+    };
+    render() {
+        return (
+            <div>
+                <InputAndButton ref={comp => { this.InputComponent = comp; }} onSave={this.props.actions.addTodo}/>
+                <LiList items={this.props.todos}/>
+            </div>
+        );
+    };
 }
 
 const mapStateToProps = state => ({
