d80447a9364a1219e3d7c0ead772cbc9f2511462
zhoutk <zhoutk@189.cn>
2017-05-23 15:50:05 +0800

    webpack dev.

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,9 @@
 let configureStore;
-configureStore = require('./configureStore.dev');
 
-// if (__DEVELOPMENT__) {
-//   configureStore = require('./configureStore.dev').default;
-// } else {
-//   configureStore = require('./configureStore.prod').default;
-// }
+if (process.env.NODE_ENV !== '__DEV__' || process.env.PLATFORM_ENV !== '__WEB__') {
+    configureStore = require('./configureStore.prod');
+} else {
+    configureStore = require('./configureStore.dev');
+}
 var {store,history} = configureStore;
 export {store,history};
